5c4d8e59217dd8a775b4aae4ed7ce53fd0167374
John Beynon <john@beynon.org.uk>
2013-08-06 19:46:14 +0100

    Copy template files from repo

--- rails4.rb
+++ rails4.rb
@@ -1,10 +1,14 @@
 # Rails 4 Application Generator
 # Resultant is 100% deployable to Heroku with no further changes
 
-
 # Helper methods
 def source_paths
   [File.expand_path(File.dirname(__FILE__))]
+end
+
+def copy_from_repo(filename, destination)
+  repo = 'https://raw.github.com/johnbeynon/heroku-rails-template/master/files/'
+  get repo + filename, destination 
 end
 
 # Add unicorn
@@ -19,16 +23,16 @@ end
 gsub_file 'Gemfile', 'sqlite3', 'pg'
 
 # Add Unicorn config
-copy_file 'files/common/config/unicorn.rb', 'config/unicorn.rb'
+copy_from_repo 'common/config/unicorn.rb', 'config/unicorn.rb'
 
 # Add a Procfile
-copy_file 'files/common/Procfile', 'Procfile'
+copy_from_repo 'common/Procfile', 'Procfile'
 
 # Remove database.yml
 remove_file 'config/database.yml'
 
 # Replace with postgres friendly database.yml
-copy_file 'files/common/config/database.yml', 'config/database.yml'
+copy_from_repo 'common/config/database.yml', 'config/database.yml'
 
 # Replace development database name with one extracted from application name
 gsub_file "config/database.yml", /database: myapp_development/, "database: #{app_name.downcase}_development"
